0539c3991077d286b6cc28a31bf200dde549c918
Alex Stanciu <stanciu.alexandru@gmail.com>
2021-04-08 11:19:43 +0300

    add deps

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,11 @@ module.exports = {
           test: /\.(js|jsx)$/,
           exclude: /node_modules/,
           use: {
-            loader: "babel-loader"
+            loader: "babel-loader",
+            options: { 
+              presets: ['@babel/preset-env', '@babel/react'],
+              plugins:['@babel/plugin-proposal-class-properties']
+            }
           }
         },
         {
